08dfd458e639d86ab2fb0a0b40d2ed336285e47f
nexusgh12 <70796139+nexusgh12@users.noreply.github.com>
2022-01-15 01:47:50 +0900

    Update BOJ_1271.java

--- BOJ_1271.java
+++ BOJ_1271.java
@@ -1,3 +1,5 @@
+package Algorithm;
+
 import java.math.BigInteger;
 import java.util.Scanner;
 
@@ -6,7 +8,7 @@ public class BOJ_1271 {
         Scanner sc = new Scanner(System.in);
         BigInteger money = sc.nextBigInteger();
         BigInteger num = sc.nextBigInteger();
-        System.out.println( money.divide(num));
+        System.out.println(money.divide(num));
         System.out.println(money.remainder(num));
     }
 }
