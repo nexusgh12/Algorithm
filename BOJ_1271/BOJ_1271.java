36634a4b04a7e7d0d4d9527b4d216a7b8c4fed26
nexusgh12 <70796139+nexusgh12@users.noreply.github.com>
2022-01-15 18:39:38 +0900

    Update BOJ_1271.java

--- BOJ_1271/BOJ_1271.java
+++ BOJ_1271/BOJ_1271.java
@@ -11,11 +11,11 @@ class BOJ_1271 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        BigInteger money =new BigInteger(st.nextToken());
-        BigInteger num =new BigInteger(st.nextToken());
+        BigInteger m =new BigInteger(st.nextToken());
+        BigInteger n =new BigInteger(st.nextToken());
 
-        bw.write(money.divide(num)+"\n");
-        bw.write(money.remainder(num)+"\n");
+        bw.write(m.divide(n)+"\n");
+        bw.write(m.remainder(n)+"\n");
 
         bw.flush();
         bw.close();
