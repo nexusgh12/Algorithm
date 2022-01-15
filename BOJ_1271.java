156269a00397967c417a82d2338d224b554dbbea
nexusgh12 <70796139+nexusgh12@users.noreply.github.com>
2022-01-15 16:02:45 +0900

    Update BOJ_1271.java
    Using buffered IO

--- BOJ_1271.java
+++ BOJ_1271.java
@@ -1,14 +1,41 @@
 package Algorithm;
 
+import java.io.*;
 import java.math.BigInteger;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
-public class BOJ_1271 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        BigInteger money = sc.nextBigInteger();
-        BigInteger num = sc.nextBigInteger();
-        System.out.println(money.divide(num));
-        System.out.println(money.remainder(num));
+//속도 84ms
+class BOJ_1271 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        BigInteger money =new BigInteger(st.nextToken());
+        BigInteger num =new BigInteger(st.nextToken());
+
+        bw.write(money.divide(num)+"\n");
+        bw.write(money.remainder(num)+"\n");
+
+        bw.flush();
+        bw.close();
+        br.close();
     }
 }
+
+//속도 136ms
+//class BOJ_1271 {
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        BigInteger money = sc.nextBigInteger();
+//        BigInteger num = sc.nextBigInteger();
+//        System.out.println(money.divide(num));
+//        System.out.println(money.remainder(num));
+//    }
+//}
+
+
+
+
+
+
