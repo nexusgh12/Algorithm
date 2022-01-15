ca005b882916e2d97f8ce140f4e89152af7e61b8
nexusgh12 <70796139+nexusgh12@users.noreply.github.com>
2022-01-15 21:12:38 +0900

    create 5554

--- BOJ_3003/BOJ_3003.java
+++ BOJ_3003/BOJ_3003.java
@@ -3,7 +3,7 @@ package Algorithm.BOJ_3003;
 import java.io.*;
 import java.util.StringTokenizer;
 
-public class BOJ_3003 {
+class BOJ_3003 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
---
+++ BOJ_5554/BOJ_5554.java
@@ -0,0 +1,28 @@
+package Algorithm.BOJ_5554;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+class BOJ_5554 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int num = 4;
+        int sum = 0;
+        StringTokenizer st = null;
+        for (int i = 0; i < num; i++)
+        {
+            st = new StringTokenizer(br.readLine());
+            sum += Integer.parseInt(st.nextToken());
+        }
+        bw.write(sum/60 + "\n");
+        bw.write(sum%60+ "\n");
+
+        bw.flush();
+        br.close();
+        bw.close();
+
+    }
+
+}
+
