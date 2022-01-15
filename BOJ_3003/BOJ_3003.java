6c460e04931b80fc4220dddd03c2c512054645ed
nexusgh12 <70796139+nexusgh12@users.noreply.github.com>
2022-01-15 19:56:04 +0900

    Update BOJ_3003.java

--- BOJ_3003/BOJ_3003.java
+++ BOJ_3003/BOJ_3003.java
@@ -8,13 +8,15 @@ public class BOJ_3003 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         int[] ch = {1, 1, 2, 2, 2, 8};
+        int len = st.countTokens();
         String s = "";
-        for(int i = 0; i<=6; i++){
-            int j = Integer.parseInt(st.nextToken());
-            bw.write((ch[i] -j) + " ");
+
+        for(int i = 0; i < len; i++){
+            s += ch[i] -  Integer.parseInt(st.nextToken()) + " ";
         }
-        //bw.write("asd\n");
+        bw.write(s);
         bw.flush();
         bw.close();
         br.close();
